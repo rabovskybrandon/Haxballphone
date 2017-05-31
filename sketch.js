9c8ae3c2c8304b6914be87aaac51205d770f9e1e
rabovskybrandon <BrandonRabovsky@Suzannes-MacBook-Pro-2.local>
2017-05-31 11:04:09 -0400

    final with kick4

--- sketch.js
+++ sketch.js
@@ -223,7 +223,7 @@ player2.bounce(wall14);
 player2.bounce(wall12top);
 player2.bounce(wall14top);
 }
-text("V6",250,250);
+text("V7",250,250);
 //Created all conditions for the reset function after a goal is scored on the red goal
 if(bluekickoff==true){
   player2.bounce(centercircle);
